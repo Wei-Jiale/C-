25191c75a9a1be4f4d15f70c48c0c4cfc7bc2304
Wei-Jiale <576401521@qq.com>
2020-02-11 22:05:09 +0800

    Sort the array by parity

---
+++ Leetcode 905/Test.cpp
@@ -0,0 +1,13 @@
+//按奇偶排序数组
+//所有偶数元素之后跟着所有奇数元素
+class Solution {
+public:
+	vector<int> sortArrayByParity(vector<int>& A) {
+		for (int i = 0, j = 0; j<A.size(); ++j){
+			if (A[j] % 2 == 0){
+				swap(A[i++], A[j]);
+			}
+		}
+		return A;
+	}
+};
