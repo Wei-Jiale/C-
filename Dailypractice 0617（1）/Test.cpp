02387014a1201512127d2521ea2c35718d765bef
Wei-Jiale <576401521@qq.com>
2020-06-26 20:27:30 +0800

    The number of 1s in binary

---
+++ Dailypractice 0617（1）/Test.cpp
@@ -0,0 +1,18 @@
+// 统计二进制位中1的个数
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	while (cin >> n){
+		int count = 0;
+		while (n){
+			if ((n & 1) == 1){
+				count++;
+			}
+			n = n >> 1;
+		}
+		cout << count << endl;
+	}
+	return 0;
+}
