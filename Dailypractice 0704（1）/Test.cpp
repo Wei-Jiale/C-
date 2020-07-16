fbb84339ca6b1c872ca541fa95c0909e21859266
Wei-Jiale <576401521@qq.com>
2020-07-16 23:52:55 +0800

    Original text and password

---
+++ Dailypractice 0704（1）/Test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	char c;
+	while ((c = getchar()) != EOF){
+		if ('A' <= c && 'Z' >= c) {
+			c = (c > 'E') ? (c - 5) : (c + 21);
+		}
+		putchar(c);
+	}
+	return 0;
+}
